af0488e54ccebb25ed55ef7107ab088147086f0e
Amir <amir1muratov@gmail.com>
2023-03-18 16:39:31 +0100

    Mask secrets

--- src/main/java/bot/Config.java
+++ src/main/java/bot/Config.java
@@ -22,8 +22,21 @@ public record Config(
     public String prettyPrint() {
         final var mapper = new ObjectMapper();
         mapper.registerModule(new Jdk8Module());
+        final var configWithoutSecrets = new Config(
+                this.tgToken.substring(0, 5) + "***",
+                this.botName,
+                this.openAiToken.substring(0, 5) + "***",
+                this.openAiUrl,
+                this.openAiModel,
+                this.openAiTemperature,
+                this.openAiTopP,
+                this.usersWhitelist,
+                this.chatGptsRole,
+                this.chatHistorySize
+        );
+
         try {
-            return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(this);
+            return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(configWithoutSecrets);
         } catch (Exception e) {
             return "";
         }
